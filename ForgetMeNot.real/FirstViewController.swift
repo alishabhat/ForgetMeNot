265a23466a9a67015edd0a6975464d0e9d8edc46
Amit Bhat <amit.bhat@berkeley.edu>
2020-07-18 12:32:41 -0700

    Began ForgetMeNot

---
+++ ForgetMeNot.real/FirstViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FirstViewController.swift
+//  ForgetMeNot.real
+//
+//  Created by Amit Bhat on 7/17/20.
+//  Copyright © 2020 alisha bridgehacks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var button1: UIButton!
+   
+    //override func didReceiveMemoryWarning() {
+      //      super.didReceiveMemoryWarning()
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        button1.applyDesign()
+           // Do any additional setup after loading the view.
+      
+    }
+
+    //override func didReceiveMemoryWarning() {
+    //    super.didReceiveMemoryWarning()
+    ////}
+    
+    @IBOutlet weak var button2: UIButton!
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    
+        button2.applyDesign()
+}
+     
+   
+    @IBOutlet weak var button3: UIButton!
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+     
+    
+        button3.applyDesign()
+    }
+   
+    @IBOutlet weak var button4: UIButton!
+    override func viewLayoutMarginsDidChange() {
+        super.viewLayoutMarginsDidChange()
+        button4.applyDesign()
+    }
+    @IBOutlet weak var button5: UIButton!
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        button5.applyDesign()
+    }
+}
+
+extension UIButton{
+    func applyDesign() {
+    self.layer.cornerRadius = 12
+
+
+
+}
+}
+
---
+++ ForgetMeNot.real/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ForgetMeNot.real
+//
+//  Created by Amit Bhat on 7/17/20.
+//  Copyright © 2020 alisha bridgehacks. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
